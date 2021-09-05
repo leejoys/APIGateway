d14db0e847a746405bdaa46a57c409b72dafd681
leejoys <44557202+leejoys@users.noreply.github.com>
2021-09-05 16:10:59 +0300

    mux start

--- main.go
+++ main.go
@@ -25,8 +25,38 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
+// Программный интерфейс сервера GoNews
+type API struct {
+	r *mux.Router
+}
+
+// Конструктор объекта API
+func New() *API {
+	api := API{}
+	api.r = mux.NewRouter()
+	api.endpoints()
+	return &api
+}
+
+// Регистрация обработчиков API.
+func (api *API) endpoints() {
+	// получить n последних новостей
+	//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
+	// веб-приложение
+	//api.r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./webapp"))))
+}
+
+// Получение маршрутизатора запросов.
+// Требуется для передачи маршрутизатора веб-серверу.
+func (api *API) Router() *mux.Router {
+	return api.r
+}
+
 func main() {
-	log.Fatal(http.ListenAndServe("localhost:8080", http.HandlerFunc()))
+	api := New()
+	log.Fatal(http.ListenAndServe("localhost:8080", api.Router))
 }
