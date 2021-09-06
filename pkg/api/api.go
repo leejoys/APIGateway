35a74fd3fbcec8df570a623c3324722f90e5aa4a
leejoys <44557202+leejoys@users.noreply.github.com>
2021-09-06 04:05:24 +0300

    memdb

--- pkg/api/api.go
+++ pkg/api/api.go
@@ -1,17 +1,25 @@
 package api
 
 import (
+	"apigateway/pkg/storage"
+	"apigateway/pkg/storage/memdb"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
 	"github.com/gorilla/mux"
 )
 
 // Программный интерфейс приложения
 type API struct {
-	r *mux.Router
+	db storage.Interface
+	r  *mux.Router
 }
 
 // Конструктор объекта API
 func New() *API {
 	api := API{}
+	api.db = memdb.New()
 	api.r = mux.NewRouter()
 	api.endpoints()
 	return &api
@@ -19,10 +27,14 @@ func New() *API {
 
 // Регистрация обработчиков API.
 func (api *API) endpoints() {
-	// получить n последних новостей
+	//метод вывода списка новостей,
+	api.r.HandleFunc("/news/latest?page={n}", api.latest).Methods(http.MethodGet)
+	//метод фильтра новостей,
 	//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
-	// веб-приложение
-	//api.r.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir("./webapp"))))
+	//метод получения детальной новости,
+	//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
+	//метод добавления комментария.
+	//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
 }
 
 // Получение маршрутизатора запросов.
@@ -30,3 +42,31 @@ func (api *API) endpoints() {
 func (api *API) Router() *mux.Router {
 	return api.r
 }
+
+//метод вывода списка новостей,
+func (api *API) latest(w http.ResponseWriter, r *http.Request) {
+	ns := mux.Vars(r)["n"]
+	n, err := strconv.Atoi(ns)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	posts, err := api.db.PostsN(n)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	bytes, err := json.Marshal(posts)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(bytes)
+}
+
+//метод фильтра новостей,
+//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
+//метод получения детальной новости,
+//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
+//метод добавления комментария.
+//api.r.HandleFunc("/news/{n}", api.posts).Methods(http.MethodGet)
